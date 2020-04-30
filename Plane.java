ba3d1cca121cb6d73c93267e47583c569c3460b3
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-04-30 21:28:18 +0530

    This is an basic java code where the system generates a response that planes can fly. Here the plane is airbus(you can name it whatever you want) and the computer gives the response "fly".

---
+++ Plane.java
@@ -0,0 +1,10 @@
+class Plane { 
+    void Fly() {
+         System.out.println("Fly");
+    }
+    
+    public static void main(String[] args) {
+        Plane airbus = new Plane();
+        airbus.Fly();
+    }
+}           
