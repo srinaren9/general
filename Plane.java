695d2cdd821530873ace50e93ce20612f390bab1
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-01 12:30:50 +0530

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -1,10 +1,10 @@
 class Plane { 
-    void Fly() {
+    void fly() {
          System.out.println("Fly");
     }
     
     public static void main(String[] args) {
         Plane airbus = new Plane();
-        airbus.Fly();
+        airbus.fly();
     }
 }           
