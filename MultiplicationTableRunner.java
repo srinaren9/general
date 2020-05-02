a8b4af492773d0e25935f85aad101dad3acd6468
ranji <ranji@localhost>
2020-05-02 16:44:14 +0530

    This is an method overloading. If you pass an argument in the second one the first one should be commented out. If the first is used(no argument) the second one should be commented out.

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,10 @@
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		//table.print();
+		table.print(9);
+	}
+
+}
