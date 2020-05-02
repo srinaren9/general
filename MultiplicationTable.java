a09c945c9830af1f32f78f29f554ac81895a70c5
ranji <ranji@localhost>
2020-05-02 16:41:16 +0530

    This project is an method overloading. If you pass argument in the second one the first one must be commented out. If you pass the no arguments in the first one the second one must be commented out and there should be no argument in the first one.

---
+++ MultiplicationTable.java
@@ -0,0 +1,17 @@
+
+public class MultiplicationTable {
+    void print() {
+	    for(int i =1; i<=100; i++) {
+		System.out.printf("%d * %d = %d", 5, i , 5*i).println();
+	    }
+    }
+	
+
+void print(int table) {
+    for(int i =1; i<=100; i++) {
+	System.out.printf("%d * %d = %d", table, i , table*i).println();
+    }
+}
+
+
+}	
