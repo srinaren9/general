22806ee4c6378dc06070ddd08fccf05b0d50f4ec
Srinaren Ayinala <ranji@localhost>
2020-05-03 17:40:21 +0530

    test

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Srinaren Ayinala
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+
+	}
+
+}
