850e81f706eebd22fa637a3e3d5d90d048895b18
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-07 20:36:27 +0530

    Add files via upload
    This is an addition runner. Any value you type it will add it from    to       .

---
+++ Addition.java
@@ -0,0 +1,8 @@
+
+public class Addition {
+       void print(int table) {
+            for(int i = 10000; i<=13456; i++)  {
+            System.out.printf("%d + %d = %d", table, i, table+i).println();	
+            }
+       }
+  }
---
+++ AdditionRunner.java
@@ -0,0 +1,10 @@
+
+public class AdditionRunner {
+
+	public static void main(String[] args) {
+	 Addition table = new Addition();
+	 table.print(89);
+
+	}
+
+}
