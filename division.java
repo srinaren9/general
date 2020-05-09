3e502c138732a1a1df8f7c8691e77a8c47535106
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-09 18:26:22 +0530

    Add files via upload
    This would divide numbers by 1 all the way to 100.(no decimals)

---
+++ division.java
@@ -0,0 +1,8 @@
+
+public class division {
+	void print(int table) {
+         for(int i=1; i<100 ; i++)  {
+         System.out.printf("%d / %d = %d", table, i , table/i).println();
+    }
+  }
+}
---
+++ divisionRunner.java
@@ -0,0 +1,9 @@
+
+public class divisionRunner {
+
+	public static void main(String[] args) {
+	  division table = new division();
+	  table.print(7456);
+	}
+
+}
