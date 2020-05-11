18019dfa47059346145f983355e0725058c1e21e
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-11 21:00:30 +0530

    Add files via upload
    This code will help you store no.of books you have :D

---
+++ Book.java
@@ -0,0 +1,8 @@
+
+public class Book {
+	private int noOfCopies;
+
+	public void setNoOfCopies(int noOfCopies) {
+		this.noOfCopies = noOfCopies;
+	}
+}
---
+++ BookRunner.java
@@ -0,0 +1,16 @@
+
+public class BookRunner {
+
+	public static void main(String[] args) {
+	     // Create a new class called Book
+		// Create three instances
+		Book artOfComputerProgramming = new Book();
+		Book aviationIsGreat = new Book();
+		Book cookingAdventures = new Book();
+	
+	    artOfComputerProgramming.setNoOfCopies(100);
+	    aviationIsGreat.setNoOfCopies(50);
+	    cookingAdventures.setNoOfCopies(40);
+	} 
+
+}
