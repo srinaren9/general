c985930c0cbe55ad3198a957c9176e29c8f0c00f
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-12 17:31:13 +0530

    Add files via upload
    This is how I am missing flying and telling that I like to fly and want to

---
+++ Fly.java
@@ -0,0 +1,14 @@
+
+public class Fly {
+	void fly() {
+	         System.out.println("i like to fly");
+	    }
+	    
+	    public static void main(String[] args) {
+	        Plane airbus = new Plane();
+	        airbus.fly();
+	    }
+	}           
+
+		
+	
