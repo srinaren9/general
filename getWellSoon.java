9f4d6abace94aea585f9b051df9a0a146d46a97b
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-13 23:05:05 +0530

    Add files via upload
    This code wishes all the COVID-19 infected people get well soon.

---
+++ getWellSoon.java
@@ -0,0 +1,12 @@
+package general;
+
+public class getWellSoon {
+	void getWellSoon() {
+        System.out.println("Get Well Soon");
+   }
+   
+   public static void main(String[] args) {
+       getWellSoon airbus = new getWellSoon();
+       airbus.getWellSoon();
+   }
+}           
