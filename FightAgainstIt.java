b9065332bcc3fc5096d9d3311512c698fcf94aaa
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-15 16:57:21 +0530

    Add files via upload

---
+++ FightAgainstIt.java
@@ -0,0 +1,16 @@
+
+
+public class FightAgainstIt {
+	void  FightAgainstIt () {
+        System.out.println("Lets fight the pandemic and be positive and safe.");
+   }
+   
+   public static void main(String[] args) {
+	   FightAgainstIt airbus = new  FightAgainstIt();
+       airbus. FightAgainstIt();
+   }
+}           
+
+	
+
+
