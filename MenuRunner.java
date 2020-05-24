80382d1694f17199c643ae6c72244c84ac2e65c7
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-05-24 17:14:50 +0530

    Add files via upload
    This class is aimed at providing calculator functions to the user for any
     2 numbers entered as input

--- MenuRunner.java
+++ MenuRunner.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/****
+ * 
+ * @author SriNaren Ayinala
+ * This class is aimed at providing calculator functions to the user for any
+ * 2 numbers entered as input
+ */
 public class MenuRunner {
 
 	public static void main(String[] args) {
@@ -27,16 +33,34 @@ public class MenuRunner {
 	     System.out.println(" Number2 = " + number2);
 	     System.out.println(" Choice =  " + choice);
 	     
-	          if(choice == 1) {
-	    	 System.out.println("Result "  + (number1 + number2));
-	     } else if(choice ==2 ) {
-	    	 System.out.println("Result "  + (number1 - number2));
-	     } else if(choice == 3) {
-	    	 System.out.println("Result "  + (number1 * number2));
-	     } else if(choice == 4) {
-	    	 System.out.println("Result "  + (number1 / number2));
-	     } else {
-	    	 System.out.println("Invalid Choice");
-	     }
-    }
+
+
+         switchOption(number1, number2, choice);
+}
+/***
+ * 
+ * @param number1 is the input 1 from user
+ * @param number2 is the input 2 from user
+ * @param choice is the calculator choice to be chosen by user. 4 options allowed
+ */
+	private static void switchOption(int number1, int number2, int choice) {
+		switch(choice ) {
+		case 1 : 
+			System.out.println("Results " + (number1 + number2));
+		    break;
+		case 2 : 
+			System.out.println("Results " + (number1 - number2));
+		    break;
+		case 3 : 
+			System.out.println("Results " + (number1 * number2));
+		    break;
+		case 4 : 
+			System.out.println("Results " + (number1 / number2));
+		    break;
+		
+		    
+		
+		
+		}
+ }
 }
