b37222ecae60c6ccb5ea7df2d1d84e0babf1c351
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-06-05 19:06:28 +0530

    Add files via upload
    This code will add 2 different binumbers then multiply then double each of there values.

---
+++ BiNumberRunner.java
@@ -0,0 +1,21 @@
+
+public class BiNumberRunner {
+	
+	public static void main(String[] args) {
+		
+		BiNumber numbers = new BiNumber(2, 3);
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+		
+		numbers.doubleValue();//Double both numbers
+		System.out.println("" + number());
+		System.out.println("" + number());
+
+
+	}
+
+	private static String number() {
+		return null;
+	}
+
+}
