3e115dcaac33b3e035179c308d7ed7f3153dbde0
Srinaren Venkat Ayinala <64528967+srinaren9@users.noreply.github.com>
2020-06-11 20:29:59 +0530

    Add files via upload
    This code takes input from the user and multiplies the respective numbers :D

---
+++ InputMultiplicationRunner.java
@@ -0,0 +1,34 @@
+/***
+ * This code take input from the user and multiplies the respective numbers
+ * @author Srinaren V Ayinala
+ *
+ */
+
+
+import java.util.Scanner;
+
+
+public class InputMultiplicationRunner {
+
+	public static void main(String[] args) {
+		 
+		Scanner scanner = new Scanner(System.in);
+		
+		 System.out.println("Enter Number1: ");
+	     int number1  = scanner.nextInt();
+	     
+	     System.out.println("Enter Number2: "); 
+	     int number2 = scanner.nextInt();
+	
+         System.out.println("Result : " + (number1 * number2));	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
